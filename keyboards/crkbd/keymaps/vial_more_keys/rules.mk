12c03ee82614657cbda53661f0b0a25a7edcf9f2
oskarclasson <classon.oskar1@gmail.com>
2025-09-08 20:57:30 +0200

    nice setup

---
+++ keyboards/crkbd/keymaps/vial_more_keys/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+MOUSEKEY_ENABLE     = no
+OLED_ENABLE         = no
+OLED_DRIVER         = ssd1306
+OLED_TRANSPORT		= i2c
+EXTRAKEY_ENABLE     = no
+COMBO_ENABLE        = no
+
+QMK_SETTINGS        = no
